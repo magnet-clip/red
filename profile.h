7578fcea4d94b4ad17439f21f33462c7eb7a020c
Rustam <rustam.guseynov@refinitiv.com>
2020-02-14 17:17:52 +0300

    Added duration logging; small ~4.3s, mid ~37sec

--- profile.h
+++ profile.h
@@ -2,6 +2,7 @@
 
 #include <chrono>
 #include <iostream>
+#include <sstream>
 #include <string>
 
 using namespace std;
@@ -28,3 +29,31 @@ private:
 #define UNIQ_ID(lineno) UNIQ_ID_IMPL(lineno)
 
 #define LOG_DURATION(message) LogDuration UNIQ_ID(__LINE__){message};
+
+struct TotalDuration {
+  string message;
+  steady_clock::duration value;
+  explicit TotalDuration(const string &msg = "")
+      : message(msg + ": "), value(0) {}
+  ~TotalDuration() {
+    ostringstream os;
+    os << message << duration_cast<milliseconds>(value).count() << " ms"
+       << endl;
+    cerr << os.str();
+  }
+};
+class AddDuration {
+public:
+  explicit AddDuration(steady_clock::duration &dest)
+      : add_to(dest), start(steady_clock::now()) {}
+
+  explicit AddDuration(TotalDuration &dest) : AddDuration(dest.value) {}
+
+  ~AddDuration() { add_to += steady_clock::now() - start; }
+
+private:
+  steady_clock::duration &add_to;
+  steady_clock::time_point start;
+};
+
+#define ADD_DURATION(value) AddDuration UNIQ_ID(__LINE__){value};
