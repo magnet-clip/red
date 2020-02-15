8152c22760a3192de783cb588aad7bcdb53804c9
Rustam Guseynov <guseynov.r.v@gmail.com>
2020-02-15 17:23:57 +0300

    refactor

--- search_server.cpp
+++ search_server.cpp
@@ -38,8 +38,18 @@ void SearchServer::AddQueriesStream(istream &query_input,
   TotalDuration sorting_2("Sorting 2");
   TotalDuration lookup("Looking up for a document");
 #endif
-  vector<size_t> docid_count(index.DocumentCount());  // document_id -> hitcount
-  vector<pair<size_t, size_t>> search_results(index.DocumentCount());
+
+  const auto comparer = [](pair<size_t, size_t> lhs, pair<size_t, size_t> rhs) {
+    int64_t lhs_docid = lhs.first;
+    auto lhs_hit_count = lhs.second;
+    int64_t rhs_docid = rhs.first;
+    auto rhs_hit_count = rhs.second;
+    return make_pair(lhs_hit_count, -lhs_docid) >
+           make_pair(rhs_hit_count, -rhs_docid);
+  };
+  const auto total_doc_count = index.DocumentCount();
+  vector<size_t> docid_count(total_doc_count);  // document_id -> hitcount
+  vector<pair<size_t, size_t>> search_results(total_doc_count);
 
   for (string current_query;
        getline(query_input, current_query);) {  // # queries <= 500k
@@ -68,11 +78,10 @@ void SearchServer::AddQueriesStream(istream &query_input,
     // # of search_results <= 50k (btw my case as all the query are found in
     // documents)
     size_t actual_count = 0;
-    // search_results.resize(index.DocumentCount());
-    fill(search_results.begin(), search_results.end(), make_pair(0, 0));
     {
       ADD_DURATION(sorting_1);
-      for (size_t i = 0; i < index.DocumentCount(); i++) {
+      fill(search_results.begin(), search_results.end(), make_pair(0, 0));
+      for (size_t i = 0; i < total_doc_count; i++) {
         if (docid_count[i] > 0) {
           search_results[actual_count++] = {i, docid_count[i]};
         }
@@ -80,38 +89,21 @@ void SearchServer::AddQueriesStream(istream &query_input,
     }
     {
       ADD_DURATION(sorting_2);
-      // search_results.resize(actual_count);
       if (actual_count < 5) {
         sort(begin(search_results), next(begin(search_results), actual_count),
-             [](pair<size_t, size_t> lhs, pair<size_t, size_t> rhs) {
-               int64_t lhs_docid = lhs.first;
-               auto lhs_hit_count = lhs.second;
-               int64_t rhs_docid = rhs.first;
-               auto rhs_hit_count = rhs.second;
-               return make_pair(lhs_hit_count, -lhs_docid) >
-                      make_pair(rhs_hit_count, -rhs_docid);
-             });
+             comparer);
       } else {
         partial_sort(begin(search_results), next(begin(search_results), 5),
-                     next(begin(search_results), actual_count),
-                     [](pair<size_t, size_t> lhs, pair<size_t, size_t> rhs) {
-                       int64_t lhs_docid = lhs.first;
-                       auto lhs_hit_count = lhs.second;
-                       int64_t rhs_docid = rhs.first;
-                       auto rhs_hit_count = rhs.second;
-                       return make_pair(lhs_hit_count, -lhs_docid) >
-                              make_pair(rhs_hit_count, -rhs_docid);
-                     });
+                     next(begin(search_results), actual_count), comparer);
       }
     }
 
     search_results_output << current_query << ':';
-    for (auto [docid, hitcount] : Head(search_results, 5)) {
-      if (hitcount > 0) {
-        search_results_output << " {"
-                              << "docid: " << docid << ", "
-                              << "hitcount: " << hitcount << '}';
-      }
+    for (auto [docid, hitcount] :
+         Head(search_results, min(actual_count, (size_t)5))) {
+      search_results_output << " {"
+                            << "docid: " << docid << ", "
+                            << "hitcount: " << hitcount << '}';
     }
     search_results_output << endl;
   }
